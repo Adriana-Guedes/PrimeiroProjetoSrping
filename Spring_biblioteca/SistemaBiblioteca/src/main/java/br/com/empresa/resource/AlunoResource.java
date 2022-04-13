4ef47b35eee6c119d9c7069a5797147b1af2c793
Adriana-Guedes <drikaguedess@gmail.com>
2022-04-13 15:18:26 -0300

    continuacao

--- Spring_biblioteca/SistemaBiblioteca/src/main/java/br/com/empresa/resource/AlunoResource.java
+++ Spring_biblioteca/SistemaBiblioteca/src/main/java/br/com/empresa/resource/AlunoResource.java
@@ -8,12 +8,10 @@ package br.com.empresa.resource;
 import java.net.URI;
 import java.util.List;
 
-import javax.servlet.Servlet;
 
 import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
